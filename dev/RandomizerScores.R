662242d84ca171ae32e5a287e5e061af6f4c9647
Carly <celevitz@gmail.com>
2024-03-10 21:38:02 -0700

    Adding Judge analysis

--- dev/RandomizerScores.R
+++ dev/RandomizerScores.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(touRnamentofchampions)
+
 results %>%
   left_join(randomizer) %>%
   select(season,episode,round,challenge,chef
@@ -29,3 +32,77 @@ results %>%
   select(season,challenge,chef,total,mintotal,randomizer1,randomizer2,randomizer3,randomizer4
          ,time,randomizer5) %>%
   filter(total == mintotal)
+
+
+
+
+#########
+## Judges
+
+judges %>%
+  select(season,episode,judge) %>%
+  distinct() %>%
+  group_by(judge) %>%
+  mutate(numberofepisodes = n()) %>%
+  select(!episode) %>%
+  distinct() %>%
+  mutate(numberofseasons=n()) %>%
+  select(judge,numberofepisodes,numberofseasons) %>%
+  distinct() %>%
+  arrange(desc(numberofseasons)) %>%
+  print(n=30)
+
+
+judges %>%
+  distinct() %>%
+  left_join(results %>% filter(!(is.na(total)))) %>%
+  # keep just the judges who have judged in at least 4 episodes
+  #filter(judge %in% c("Nancy Silverton","Ming Tsai","Jonathan Waxman"
+  #                    ,"Traci Des Jardins","Rocco DiSpirito","Scott Conant"
+  #                    ,"Marcus Samuelsson","Giada de Laurentiis"
+  #                    ,"Alex Guarnaschelli","Lorena Garcia","Cat Cora"
+  #                    ,"Andrew Zimmern")) %>%
+  group_by(judge) %>%
+  summarise(averagescore=mean(total,na.rm=T)) %>%
+  arrange(desc(averagescore)) %>%
+  print(n=30)
+
+
+judges %>%
+  distinct() %>%
+  left_join(results %>% filter(!(is.na(total)))) %>%
+  #filter(judge %in% c("Nancy Silverton")) %>%
+  group_by(judge) %>%
+  select(season,episode,round,judge,chef) %>%
+  distinct() %>%
+  summarise(numberofchefs =n()) %>%
+  arrange(desc(numberofchefs))
+
+####################
+## Carlos, Shota, Crista, Michael, Chris Scott, Tiffani, Dale, Karen
+
+results %>%
+  filter(chef %in% c("Carlos Anthony","Shota Nakajima","Crista Luedtke"
+                     ,"Michael Voltaggio","Chris Scott","Tiffani Faison"
+                     ,"Dale Talde","Karen Akunowicz")) %>%
+  filter(!(is.na(total)) &
+           !(round %in% c("Qualifier final","Qualifier semi-final"))
+         & season != 5) %>%
+  left_join(seeds) %>%
+  group_by(chef) %>%
+  mutate(winnernumber = ifelse(winner == "Winner",1,0)
+         ,battles=n(),averagescore=mean(total,na.rm=T)
+          ,wins=sum(winnernumber,na.rm=T),averageseed=mean(seed,na.rm=T)) %>%
+  select(season,chef,battles,averagescore,wins,averageseed) %>%
+  distinct() %>%
+  mutate(numberofseasons = n()) %>%
+  select(chef,battles,averagescore,wins,averageseed,numberofseasons) %>%
+  distinct() %>%
+  arrange(desc(chef))
+
+
+
+
+
+
+
